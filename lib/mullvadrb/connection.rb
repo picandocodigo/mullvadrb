6c328c2c09bab0be07192358bf0b59357d113d8f
Fernando Briano <fernando@picandocodigo.net>
2025-08-04 22:02:28 +0100

    Better status format and message for blocked

--- lib/mullvadrb/connection.rb
+++ lib/mullvadrb/connection.rb
@@ -1,3 +1,5 @@
+require 'countries'
+
 module Mullvadrb
   #
   # Manage Mullvad connect, disconnect and status
@@ -31,12 +33,15 @@ module Mullvadrb
           status.gsub!('Disconnected', I18n.t(:disconnected))
                 .gsub!(/$/, "\n")
         elsif status.start_with?('Connected')
-          status = status.split("\n")
-                         .sort
-                         .reject { |a| a == 'Connected' }
-                         .prepend(I18n.t(:connected))
+          status = status.split("\n").reject { |a| a == 'Connected' }
+          country_name = status.find { |a| a.match?('Visible location') }.split("\s")[2].gsub(',','')
+          country = ISO3166::Country.find_country_by_any_name(country_name)
+          status = status.prepend("#{I18n.t(:connected)} to #{country.emoji_flag} #{country.common_name}\n")
                          .push("\n")
                          .join("\n")
+        # Blocked: Failure to generate tunnel parameters: Failure to select a matching tunnel relay
+        elsif status.start_with?('Blocked')
+          status.prepend("\n🚧 ").concat("\n")
         elsif status.start_with?('Connecting')
           status = status.split("\n")
                          .sort
